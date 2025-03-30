610ae8d8ed0e0f48d2a85e2bc15e64db5df18073
Thulasi-vignan <thulasivignanbolisetty@gmail.com>
2025-03-30 15:42:21 +0530

    frequency_divider.v

--- frequency_divider.v
+++ frequency_divider.v
@@ -22,23 +22,34 @@
 
 module frequency_divid(
 input clk,reset,
-output reg clk_out
+output reg clk_1hz , clk_out
     );
     
 //    parameter divisor = 50000000;
-    reg[31:0] counter;
+    reg[31:0] counter1 , counter2;
     always@(posedge clk or posedge reset) begin
         if(reset) begin 
-            counter <=0;
-            clk_out<=0;
+            counter1 <=0;
+            clk_1hz<=0;
             end
         else if(counter == 49999999)begin 
-              counter<=0;
-              clk_out=~clk_out;
+              counter1<=0;
+              clk_1hz=~clk_1hz;
               end
          else 
-            counter <= counter + 1;
+            counter1 <= counter1 + 1;
       end         
             
-            
+      always@(posedge clk or posedge reset)begin 
+      	if(reset)begin 
+      	       counter2 <=0;	      
+		clk_out <=0;
+		end 
+		else if (counter2 == 49999)begin 
+			counter2 <=0;
+			clk_out = ~clk_out;
+		end 
+		else 
+			counter2 <= counter2 + 1;
+	end				            
 endmodule
